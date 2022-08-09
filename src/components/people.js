2163a0813f30ea711a93fe28c7252664f90ee0a9
Russell-Batt <russellbatt90@gmail.com>
2022-08-09 23:08:06 +0100

    Added company role checkboxes

--- src/components/people.js
+++ src/components/people.js
@@ -1,24 +1,65 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 function People(props) {
     const { people, setPeople } = props;
 
     const [ personName, setPersonName ] = useState('');
     const [ email, setEmail ] = useState ('');
+    const [ errorMessage, setErrorMessage ] = useState (null);
+    const companyRoles = ['Manager', 'Admin', 'Contributor'];
+    const [checked, setChecked] = useState([]);
+
+    const handleCheck = (event) => {
+        let updatedList = [...checked];
+        if (event.target.checked) {
+            updatedList = [...checked, (event.target.value)];
+        } else {
+            updatedList.splice(checked.indexOf(event.target.value), 1);
+        }
+        setChecked(updatedList);
+    };
 
     const addPerson = () => {
         setPeople([
             ...people,
             {
                 'personName': personName,
-                'email': email
+                'email': email,
+                'company_roles' : checked 
             }
         ])
     }
 
+    useEffect (() => {
+        console.log('Component mounted')
+    }, [])
+    
+    useEffect (() => {
+        console.log(checked)
+    }, [checked])
+
     const submitHandler = (e) => {
         e.preventDefault();
-        addPerson();
+
+        function validateInputs() {
+            const regex = /^([^0-9]*)$/;
+            const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+
+            if(regex.test(personName)) {
+                addPerson()
+                setErrorMessage(null);
+            }else {
+                return setErrorMessage('Please only use letters');
+            }
+            
+            if(emailRegex.test(email)) {
+                addPerson()
+                setErrorMessage(null);
+            }else {
+                return setErrorMessage('Please enter a valid email address');
+            }
+        }
+        validateInputs();
     }
 
     return (
@@ -27,21 +68,32 @@ function People(props) {
                 <div>
                     <label>
                         Name:
-                        <input type="text" name="name" value={personName} onChange={(e) => setPersonName(e.target.value)} /> 
+                        <input type="text" name="name" value={personName} onChange={(e) => setPersonName(e.target.value)} required /> 
                     </label>
                     <label>
                         Email:
-                        <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
                     </label>
-                    <button type="submit">Add Person</button>
                 </div>
+                <div>
+                    {companyRoles.map((item, index) =>
+                        <div key={index}>
+                            <input value={item} type="checkbox" onChange={handleCheck} />
+                            <span>{item}</span>
+                        </div>
+                    )}
+                </div>
+                    <button type="submit">Add Person</button>
+                    {errorMessage && <p>{errorMessage}</p>}
             </form>
+            
             <div className="people-grid">
                 {people.map((person, i) => {
                     return (
                         <div key={`person-${i}`}>
-                            <div className="grid-item">{person.personName}</div>
-                            <div className="grid-item">{person.email}</div>
+                            <p>{person.personName}</p>
+                            <p>{person.email}</p>
+                            <p>{person.company_roles.toString()}</p>
                         </div>
                     )
                 })}
--- src/components/team.js
+++ src/components/team.js
@@ -3,9 +3,9 @@ import { useState } from 'react'
 
 function Team () {
     const initialState = [
-            {personName: 'John Doe', email: 'johnd@gmail.com'},
-            {personName: 'Jane Doe', email: 'janed@gmail.com'},
-            {personName: 'Someone', email: 'someone@gmail.com'}
+            {personName: 'John Doe', email: 'johnd@gmail.com', company_roles: []},
+            {personName: 'Jane Doe', email: 'janed@gmail.com', company_roles: []},
+            {personName: 'Someone', email: 'someone@gmail.com', company_roles: []}
         ]
         const [people, setPeople] = useState(initialState);
 
